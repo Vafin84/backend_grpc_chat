b55c7672fb3a35ccaabcc2b04388a8f836c3c82d
Ruslan Vafin <vafinruslan-56@yandex.ru>
2023-11-08 16:17:14 +0300

    Added service chats in docker-compose

--- chats/lib/data/db.dart
+++ chats/lib/data/db.dart
@@ -2,10 +2,4 @@ import 'package:stormberry/stormberry.dart';
 
 late Database db;
 
-Database initDatebse() => Database(
-      host: "127.0.0.1",
-      port: 4501,
-      useSSL: false,
-      user: "vafin",
-      password: "vafin",
-    );
+Database initDatebse() => Database();
